9c78817a70c582908907d9bd09b8f52b10ab2ac7
Jared Skinner <jaredds66@gmail.com>
2015-11-03 09:13:15 -0600

    added script

---
+++ pomodoro.sh
@@ -0,0 +1,50 @@
+#! /bin/bash
+
+#this script is used as a timer for the pomororo method.
+
+count="$1"
+
+notify-send "break! - 5 minutes remaining"
+
+sleep 60
+notify-send "4 minutes remaining"
+
+sleep 60
+notify-send "3 minutes remaining"
+
+sleep 60
+notify-send "2 minutes remaining"
+
+sleep 60
+notify-send "1 minutes remaining"
+
+sleep 60
+notify-send "back to work!"
+
+
+
+for (( i = 1; i <= $count; i++))
+do
+	notify-send "start working!"
+	sleep 1500
+
+	if ( i < $count )
+	then
+		notify-send "break! - 5 minutes remaining"
+
+		sleep 60
+		notify-send "4 minutes remaining"
+
+		sleep 60
+		notify-send "3 minutes remaining"
+
+		sleep 60
+		notify-send "2 minutes remaining"
+
+		sleep 60
+		notify-send "1 minutes remaining"
+
+		sleep 60
+		notify-send "back to work!"
+	fi
+done
